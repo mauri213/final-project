f7fc5b0e13921488681a437b9a29038b56ccbf5e
Mauricio Orozco <Mauricio Orozco>
2016-12-13 12:00:10 -0500

    Added coding for photo gallery

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -57,3 +57,24 @@ module.exports = Home;
 //     }
 //   });
 // }
+
+//photo gallery
+// var $overlay = $('<div id="overlay"></div>');
+// var $image = $("<img>");
+
+// $overlay.append($image);
+
+// $("body").append($overlay);
+
+//   $("#photo-gallery a").click( function(event) {
+//     event.preventDefault();
+//     var imageLocation = $(this).attr("href");
+
+//     $image.attr("src", imageLocation);
+
+//     $overlay.show();
+//   } );
+
+//   $("#overlay").click(function() {
+//     $( "#overlay" ).hide();
+//   });
